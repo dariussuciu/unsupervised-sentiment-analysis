2d2ecfd83abf403dfde23df39e2f5e87c6e942ee
alexcosma <alexandru.cosma@catalysts.cc>
2014-02-11 17:57:12 +0200

    -Added missed class

---
+++ MainModule/source/com/unsupervisedsentiment/analysis/model/Word.java
@@ -0,0 +1,70 @@
+package com.unsupervisedsentiment.analysis.model;
+
+public class Word {
+	private String posTag;
+	private String value;
+
+	public String getPosTag() {
+		return posTag;
+	}
+
+	public void setPosTag(String posTag) {
+		this.posTag = posTag;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	public Word(String posTag, String value) {
+		super();
+		this.posTag = posTag;
+		this.value = value;
+	}
+	
+	public Word(String posTag) {
+		super();
+		this.posTag = posTag;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((posTag == null) ? 0 : posTag.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Word other = (Word) obj;
+		if (posTag == null) {
+			if (other.posTag != null)
+				return false;
+		} else if (!posTag.equals(other.posTag))
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+}
