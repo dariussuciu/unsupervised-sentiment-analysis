ddf2458181772d48ef8aa0127ab838fe4d2d5ab9
alex.cosma <alexx.cosma@gmail.com>
2014-02-25 19:05:41 +0200

    - added reading from file.

--- MainModule/source/com/unsupervisedsentiment/analysis/core/Main.java
+++ MainModule/source/com/unsupervisedsentiment/analysis/core/Main.java
@@ -11,6 +11,7 @@ import com.unsupervisedsentiment.analysis.model.Triple;
 import com.unsupervisedsentiment.analysis.model.Tuple;
 import com.unsupervisedsentiment.analysis.model.TupleType;
 import com.unsupervisedsentiment.analysis.model.Word;
+import com.unsupervisedsentiment.analysis.modules.IO.Input.InputService;
 import com.unsupervisedsentiment.analysis.modules.IO.Input.Models.Config;
 import com.unsupervisedsentiment.analysis.modules.doublepropagation.DoublePropagationAlgorithm;
 
@@ -23,95 +24,99 @@ public class Main {
 		Config config = Initializer.getConfig();
 		//System.out.println(Config.getInputDirectory());
 		
-		DoublePropagationData inputData = new DoublePropagationData();
-		// inputData.setInput(StanfordNLPTestConstants.SENTENCE_LIU);
-		// inputData.setInput(StanfordNLPTestConstants.SENTENCE_TEST1);
-		// inputData.setInput(StanfordNLPTestConstants.SENTENCE_TEST2);
-		// inputData.setInput(StanfordNLPTestConstants.SENTENCE_TEST3);
-		// inputData.setInput(StanfordNLPTestConstants.SENTENCE_TEST4);
-		// inputData.setInput(StanfordNLPTestConstants.SENTENCE_TEST5);
-		// inputData.setInput(StanfordNLPTestConstants.SENTENCE_TEST6);
-		// inputData.setInput(StanfordNLPTestConstants.SMALL_TEST);
-		inputData.setInput(StanfordNLPTestConstants.HUGE_REVIEW);
-
-//		 inputData.setInput(StanfordNLPTestConstants.SENTENCE_TEST1 + " " +
-//		 StanfordNLPTestConstants.SENTENCE_TEST2
-//		 + " " + StanfordNLPTestConstants.SENTENCE_TEST3 + " " +
-//		 StanfordNLPTestConstants.SENTENCE_TEST4 + " "
-//		 + StanfordNLPTestConstants.SENTENCE_TEST5 + " " +
-//		 StanfordNLPTestConstants.SENTENCE_TEST6);
-		 
-		DoublePropagationAlgorithm algorithm = new DoublePropagationAlgorithm(inputData);
-
-		HashSet<Tuple> seedWords = new HashSet<Tuple>();
-
-//		Tuple test = new Tuple();
-//
-//		test.setSource(new Word("JJ", "good", ElementType.OPINION_WORD));
-//		test.setTupleType(TupleType.Seed);
-//		seedWords.add(test);
-
-//		Tuple test2 = new Tuple();
-//
-//		test2.setSource(new Word("JJ", "best", ElementType.OPINION_WORD));
-//		test2.setTupleType(TupleType.Seed);
-//
-//		seed.add(test2);
-//
-//		Tuple test3 = new Tuple();
-//
-//		test3.setSource(new Word("JJ", "great", ElementType.OPINION_WORD));
-//		test3.setTupleType(TupleType.Seed);
-//
-//		seed.add(test3);
-//
-//		Tuple test4 = new Tuple();
-//
-//		test4.setSource(new Word("JJ", "amazed", ElementType.OPINION_WORD));
-//		test4.setTupleType(TupleType.Seed);
-//
-//		seed.add(test4);
-//
-//		Tuple test5 = new Tuple();
-//
-//		test5.setSource(new Word("JJ", "dramatic", ElementType.OPINION_WORD));
-//		test5.setTupleType(TupleType.Seed);
-//
-//		seed.add(test5);
-//
-//		Tuple test6 = new Tuple();
-//
-//		test6.setSource(new Word("JJ", "powerful", ElementType.OPINION_WORD));
-//		test6.setTupleType(TupleType.Seed);
-//
-//		seed.add(test6);
-//
-//		Tuple test7 = new Tuple();
-//
-//		test7.setSource(new Word("JJ", "slow", ElementType.OPINION_WORD));
-//		test7.setTupleType(TupleType.Seed);
-//
-//		seed.add(test7);
+		InputService inputService = InputService.getInstance(config);
 		
-		for(String seedString : config.getSeedWords())
-		{
-			Tuple seed = new Tuple();
-			Word word = new Word("JJ", seedString.trim(), ElementType.OPINION_WORD);
-			seed.setSource(word);
-			seed.setTupleType(TupleType.Seed);
-			seedWords.add(seed);
-		}
-
-		long currentTime = System.currentTimeMillis();
-		algorithm.execute(seedWords);
-		System.out.println("Elapsed time: " + (System.currentTimeMillis() - currentTime) + " ms");
-
-		System.out.println("-----------------------------------------");
-		System.out.println("Features");
-		PreetyPrintTuples(algorithm.getData().getFeatureTuples());
-		System.out.println("-----------------------------------------");
-		System.out.println("OpinionWords");
-		PreetyPrintTuples(algorithm.getData().getExpandedOpinionWordsTuples());
+		inputService.getTextFromFile();
+		
+//		DoublePropagationData inputData = new DoublePropagationData();
+//		// inputData.setInput(StanfordNLPTestConstants.SENTENCE_LIU);
+//		// inputData.setInput(StanfordNLPTestConstants.SENTENCE_TEST1);
+//		// inputData.setInput(StanfordNLPTestConstants.SENTENCE_TEST2);
+//		// inputData.setInput(StanfordNLPTestConstants.SENTENCE_TEST3);
+//		// inputData.setInput(StanfordNLPTestConstants.SENTENCE_TEST4);
+//		// inputData.setInput(StanfordNLPTestConstants.SENTENCE_TEST5);
+//		// inputData.setInput(StanfordNLPTestConstants.SENTENCE_TEST6);
+//		// inputData.setInput(StanfordNLPTestConstants.SMALL_TEST);
+//		inputData.setInput(StanfordNLPTestConstants.HUGE_REVIEW);
+//
+////		 inputData.setInput(StanfordNLPTestConstants.SENTENCE_TEST1 + " " +
+////		 StanfordNLPTestConstants.SENTENCE_TEST2
+////		 + " " + StanfordNLPTestConstants.SENTENCE_TEST3 + " " +
+////		 StanfordNLPTestConstants.SENTENCE_TEST4 + " "
+////		 + StanfordNLPTestConstants.SENTENCE_TEST5 + " " +
+////		 StanfordNLPTestConstants.SENTENCE_TEST6);
+//		 
+//		DoublePropagationAlgorithm algorithm = new DoublePropagationAlgorithm(inputData);
+//
+//		HashSet<Tuple> seedWords = new HashSet<Tuple>();
+//
+////		Tuple test = new Tuple();
+////
+////		test.setSource(new Word("JJ", "good", ElementType.OPINION_WORD));
+////		test.setTupleType(TupleType.Seed);
+////		seedWords.add(test);
+//
+////		Tuple test2 = new Tuple();
+////
+////		test2.setSource(new Word("JJ", "best", ElementType.OPINION_WORD));
+////		test2.setTupleType(TupleType.Seed);
+////
+////		seed.add(test2);
+////
+////		Tuple test3 = new Tuple();
+////
+////		test3.setSource(new Word("JJ", "great", ElementType.OPINION_WORD));
+////		test3.setTupleType(TupleType.Seed);
+////
+////		seed.add(test3);
+////
+////		Tuple test4 = new Tuple();
+////
+////		test4.setSource(new Word("JJ", "amazed", ElementType.OPINION_WORD));
+////		test4.setTupleType(TupleType.Seed);
+////
+////		seed.add(test4);
+////
+////		Tuple test5 = new Tuple();
+////
+////		test5.setSource(new Word("JJ", "dramatic", ElementType.OPINION_WORD));
+////		test5.setTupleType(TupleType.Seed);
+////
+////		seed.add(test5);
+////
+////		Tuple test6 = new Tuple();
+////
+////		test6.setSource(new Word("JJ", "powerful", ElementType.OPINION_WORD));
+////		test6.setTupleType(TupleType.Seed);
+////
+////		seed.add(test6);
+////
+////		Tuple test7 = new Tuple();
+////
+////		test7.setSource(new Word("JJ", "slow", ElementType.OPINION_WORD));
+////		test7.setTupleType(TupleType.Seed);
+////
+////		seed.add(test7);
+//		
+//		for(String seedString : config.getSeedWords())
+//		{
+//			Tuple seed = new Tuple();
+//			Word word = new Word("JJ", seedString.trim(), ElementType.OPINION_WORD);
+//			seed.setSource(word);
+//			seed.setTupleType(TupleType.Seed);
+//			seedWords.add(seed);
+//		}
+//
+//		long currentTime = System.currentTimeMillis();
+//		algorithm.execute(seedWords);
+//		System.out.println("Elapsed time: " + (System.currentTimeMillis() - currentTime) + " ms");
+//
+//		System.out.println("-----------------------------------------");
+//		System.out.println("Features");
+//		PreetyPrintTuples(algorithm.getData().getFeatureTuples());
+//		System.out.println("-----------------------------------------");
+//		System.out.println("OpinionWords");
+//		PreetyPrintTuples(algorithm.getData().getExpandedOpinionWordsTuples());
 	}
 
 	private static void PreetyPrintTuples(Set<Tuple> tuples) {
--- MainModule/source/com/unsupervisedsentiment/analysis/modules/IO/Input/InputService.java
+++ MainModule/source/com/unsupervisedsentiment/analysis/modules/IO/Input/InputService.java
@@ -1,8 +1,83 @@
 package com.unsupervisedsentiment.analysis.modules.IO.Input;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.unsupervisedsentiment.analysis.modules.IO.Input.Models.Config;
+
 public class InputService {
-	public void GetTextFromFile()
-	{
-		
+
+	private Config config;
+	private static InputService inputService;
+
+	public static InputService getInstance(Config config) {
+		if (inputService == null)
+			inputService = new InputService(config);
+		return inputService;
+	}
+
+	private InputService(Config config) {
+		this.config = config;
+	}
+
+	public List<InputWrapper> getTextFromFile() {
+		// File folder = new File(config.getInputDirectory());
+		File folder = new File(
+				"C:\\Users\\Alex\\Desktop\\Research\\Project\\Input");
+
+		File[] listOfFiles = folder.listFiles();
+		List<InputWrapper> input = new ArrayList<InputWrapper>();
+
+		for (File file : listOfFiles) {
+			if (file.isFile()) {
+				input.add(getInputFromFile(file));
+			}
+		}
+		return input;
+	}
+
+	private InputWrapper getInputFromFile(File file) {
+		try {
+			Scanner in = new Scanner(new FileReader(file.getPath()));
+			InputWrapper iWrapper = new InputWrapper();
+			getHeaders(in, iWrapper);
+			getContent(in, iWrapper);
+			return iWrapper;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private void getContent(Scanner in, InputWrapper iWrapper) {
+		StringBuilder content = new StringBuilder("");
+		while (in.hasNextLine()) {
+			content.append(in.nextLine());
+			content.append(" ");
+		}
+		iWrapper.setContent(content.toString());
+	}
+
+	private void getHeaders(Scanner in, InputWrapper iWrapper) {
+		if (in.hasNextLine()) {
+			String author = in.nextLine();
+			iWrapper.setAuthor(getFormattedHeaderInfo(author));
+		}
+		if (in.hasNextLine()) {
+			String source = in.nextLine();
+			iWrapper.setSource(getFormattedHeaderInfo(source));
+		}
+	}
+
+	private String getFormattedHeaderInfo(String header) {
+		header = header.split("//")[1];
+		if (header.charAt(0) == ' ')
+			header = header.substring(1, header.length());
+		return header;
 	}
 }
