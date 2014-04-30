8eb3ed10e2679a92e377e88827dbe47e30a21f94
darius.suciu04 <darius.suciu04@gmail.comgit config --global user.email darius.suciu04@gmail.comgit config --global user.email darius.suciu04@gmail.com>
2014-04-30 11:08:17 +0300

    -Added rules for target extraction

--- MainModule/source/com/unsupervisedsentiment/analysis/modules/doublepropagation/Helpers.java
+++ MainModule/source/com/unsupervisedsentiment/analysis/modules/doublepropagation/Helpers.java
@@ -341,14 +341,14 @@ public class Helpers {
 		if(Pos_NNRel.getInstance().contains(H.tag()) && Pos_NNRel.getInstance().contains(target.tag()))
 		return false;
 		
-		//if(Pos_NNRel.getInstance().contains(H.tag()) && Pos_NNRel.getInstance().contains(source.getPosTag()))
-		//return false;
+		if(Pos_NNRel.getInstance().contains(H.tag()) && Pos_NNRel.getInstance().contains(source.getPosTag()))
+		return false;
 		
 		if(Pos_JJRel.getInstance().contains(H.tag()) && Pos_JJRel.getInstance().contains(target.tag()))
 		return false;
 		
-		//if(Pos_JJRel.getInstance().contains(H.tag()) && Pos_JJRel.getInstance().contains(source.getPosTag()))
-		//return false;
+		if(Pos_JJRel.getInstance().contains(H.tag()) && Pos_JJRel.getInstance().contains(source.getPosTag()))
+		return false;
 
 		return true;
 	}
